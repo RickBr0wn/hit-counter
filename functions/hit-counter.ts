37cd36098a5d490bcb695f84a10c18c1542d9303
Rick Brown <ricky.brown.00@gmail.com>
2022-12-15 16:41:49 +0000

    reverts back to .env method

--- functions/hit-counter.ts
+++ functions/hit-counter.ts
@@ -1,16 +1,15 @@
-import { ENV } from './config'
 import { APIGatewayEvent } from 'aws-lambda'
 import { initializeApp } from 'firebase/app'
 import { doc, getFirestore, increment, updateDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: ENV.FIREBASE_API_KEY,
-  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
-  projectId: ENV.FIREBASE_PROJECT_ID,
-  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
-  appId: ENV.FIREBASE_APP_ID,
-  measurementId: ENV.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 }
 
 initializeApp(firebaseConfig)
@@ -31,7 +30,7 @@ export async function handler(event: APIGatewayEvent) {
 
   const param = path.replace('/', '')
 
-  const docRef = doc(db, ENV.COLLECTION_NAME, param)
+  const docRef = doc(db, process.env.COLLECTION_NAME, param)
 
   await updateDoc(docRef, {
     hits: increment(1),
